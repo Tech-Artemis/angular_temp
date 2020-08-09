d8a66afabd8726ca86382f6d41531803a157836a
Tech-Artemis <techartemis@outlook.com>
2020-08-09 21:22:29 +0530

    Converted the sidebar into angular component

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { LayoutComponent } from './layout/layout.component';
-import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
-  declarations: [SidebarComponent, LayoutComponent, TopbarComponent],
+  declarations: [
+    SidebarComponent,
+    LayoutComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    LayoutComponent
   ]
 })
 export class CoreModule { }
