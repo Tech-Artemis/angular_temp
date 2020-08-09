3d7844018a4283f8a4cf193f0a76d0ce8758d2a7
Tech-Artemis <techartemis@outlook.com>
2020-08-09 20:26:22 +0530

    Skeleton for layout

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LayoutComponent } from './layout/layout.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SidebarComponent, LayoutComponent, TopbarComponent],
   imports: [
     CommonModule
   ]
