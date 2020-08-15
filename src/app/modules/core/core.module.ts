890823ff567668db2db4b1b36a4dccffc6d8e41b
Tech-Artemis <techartemis@outlook.com>
2020-08-15 21:35:00 +0530

    Created components for search, footer & newsletter

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    NewsletterComponent,
+    FooterComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    SidebarComponent
+    SidebarComponent,
+    NewsletterComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
