a22e4c7adacf582a8d5a8f425377cf067988ab08
Tech-Artemis <techartemis@outlook.com>
2020-08-15 21:00:06 +0530

    Moved Home details to its component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    SidebarComponent
-  ],
+  declarations: [HomeComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    SidebarComponent
+    HomeComponent
   ]
 })
-export class CoreModule { }
+export class HomeModule { }
