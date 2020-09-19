0349fad167a8c3d16954901d9050dd87a8bc41c2
Tech-Artemis <techartemis@outlook.com>
2020-09-19 21:16:50 +0530

    #3 - Resize images square & apply shadow on images

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -10,15 +10,15 @@ export class HomeComponent implements OnInit {
   images = [
     'assets/img/Products/room-freshner/scent-wali-room-freshner-aromatic-diffuser-set-room-freshener.jpg',
     'assets/img/Products/room-freshner/scent-wali-room-freshner-green-apple-room-freshener.jpg',
-    'assets/img/Products/luxury/scent-wali-luxury-purse-concentrate.jpg',
-    'assets/img/Products/eau-de-parfum/scent-wali-eau-de-parfum-30-ml-pomegranate-berry.jpg',
-    'assets/img/Products/eau-de-parfum/scent-wali-eau-de-parfum-60-ml-blue-lagoon.jpg',
-    'assets/img/Products/after-shave/scent-wali-after-shave-lotion.jpg',
-    'assets/img/Products/eau-de-parfum/scent-wali-eau-de-parfum-30-ml-blue-lagoon.jpg',
-    'assets/img/Products/eau-de-parfum/scent-wali-eau-de-parfum-60-ml-pomegranate-berry.jpg',
-    'assets/img/Products/eau-de-parfum/scent-wali-eau-de-parfum-attar-non-alcoholic-perfume.jpg',
-    'assets/img/Products/medi-care/scent-wali-medi-care-herbal-insect-repellent.jpg',
-    'assets/img/Products/medi-care/scent-wali-medi-care-roll-on-for-headache.jpg',
+    'assets/img/Products/luxury/scent-wali-luxury-purse-concentrate-squared.jpg',
+    'assets/img/Products/eau-de-parfum/scent-wali-eau-de-parfum-30-ml-pomegranate-berry-squared.jpg',
+    'assets/img/Products/eau-de-parfum/scent-wali-eau-de-parfum-60-ml-blue-lagoon-squared.jpg',
+    'assets/img/Products/after-shave/scent-wali-after-shave-lotion-squared.jpg',
+    'assets/img/Products/eau-de-parfum/scent-wali-eau-de-parfum-30-ml-blue-lagoon-squared.jpg',
+    'assets/img/Products/eau-de-parfum/scent-wali-eau-de-parfum-60-ml-pomegranate-berry-squared.jpg',
+    'assets/img/Products/eau-de-parfum/scent-wali-eau-de-parfum-attar-non-alcoholic-perfume-squared.jpg',
+    'assets/img/Products/medi-care/scent-wali-medi-care-herbal-insect-repellent-squared.jpg',
+    'assets/img/Products/medi-care/scent-wali-medi-care-roll-on-for-headache-squared.jpg',
   ];
 
   constructor() { }
